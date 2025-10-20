b137f758d84d84a9d22e18ca2288f33a8d728256
Datan <Datan.ctupide@yandex.ru>
2025-10-20 12:54:45 +0430

    Add files via upload

---
+++ СОЧ/script.js
@@ -0,0 +1,11 @@
+let log = document.getElementById(l)
+let pswd = document.getElementById(p)
+let btn = document.getElementById(b)
+
+function user(){
+    if (!log || !pswd){
+        alert("Неправиьно введен логин или пароль");
+        return;
+    }
+    else {}
+}
