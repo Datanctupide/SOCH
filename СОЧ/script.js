02aa4077994e0e4c858dea9f5f37208a750b190c
Datan <Datan.ctupide@yandex.ru>
2025-10-22 10:37:25 +0430

    Add files via upload

--- СОЧ/script.js
+++ СОЧ/script.js
@@ -1,11 +1,27 @@
-let log = document.getElementById(l)
-let pswd = document.getElementById(p)
 let btn = document.getElementById(b)
+let div = document.getElementById(d1)
+let div2 = document.getElementById(d2)
 
-function user(){
-    if (!log || !pswd){
-        alert("Неправиьно введен логин или пароль");
-        return;
-    }
-    else {}
-}
+btn.addEventListener("click", function () {
+  const loginInput = document.getElementById(l).value;
+  const passwordInput = document.getElementById(p).value;
+
+  fetch('users.json')
+    .then(users => {
+      const foundUser = users.find(user =>
+        user.login === loginInput && user.password === passwordInput
+      );
+
+      if (foundUser) {
+        div.hidden = true;
+        div2.hidden = false;
+      } 
+      else {
+        div.hidden = false;
+        div2.hidden = true;
+      }
+    })
+    .catch(error => {
+      console.error("Ошибка:", error);
+    });
+});
